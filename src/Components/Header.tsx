69e7d83b84255562b12ac74da3bc47b911bfc532
ThalesJC <thales.soul96@gmail.com>
2023-01-14 18:45:28 -0300

    feat: text state on input

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
 
 function Header() {
+    const [text, setText] = useState<string | undefined>();
+
     return(
         <>
+
         <h1>ReactTS To-do-list</h1>
-        <input type="text" />
+
+        <input
+            type="text"
+            value={ text }
+            onChange={ (e) => setText(e.target.value) }
+        />
+
         <button type="button">Adicionar Tarefa</button>
+
         <button type="button">Excluir tudo</button>
+
         </>
     )
 }
--- src/context/hook.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react"
-import MyContext from './myContext'
-
-export const useContextHook = () => {
-    return useContext(MyContext)
-}
